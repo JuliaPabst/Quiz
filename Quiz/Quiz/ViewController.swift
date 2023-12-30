ff70dbff2c5d0bd115bc107957c835c0a73aa6b3
Jules <ju.pabst@gmail.com>
2023-12-30 13:37:39 +0100

    added outlets, actions and question array

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let questions = ["Question 1", "Question 2", "Question 3"]
+    
+    var currentQuestion = 0
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var trueButton: UIButton!
+    @IBOutlet var falseButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        if currentQuestion < questions.count - 1 {
+            currentQuestion += 1
+            updateUI()
+            progressBar.progress = Float(currentQuestion) / Float(questions.count)
+        }
+    }
+    
+    func updateUI (){
+        questionLabel.text = questions[currentQuestion]
+    }
+    
 }
 
