a75d2fd14ee186733ef53fd35cb3308c368bcd40
Jules <ju.pabst@gmail.com>
2023-12-31 11:04:20 +0100

    changed questions array to structure, added animation if button is pressed

---
+++ Quiz/Quiz/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  Quiz
+//
+//  Created by Julia Pabst on 31.12.23.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: Int
+}
--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let questions = ["Question 1", "Question 2", "Question 3"]
+    let questions: [Question] = [Question(text: "Question 1", answer: 1), Question(text: "Question 2", answer: 0), Question(text: "Question 3", answer: 1)]
     
     var currentQuestion = 0
+    
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var progressBar: UIProgressView!
     @IBOutlet var trueButton: UIButton!
@@ -25,15 +26,27 @@ class ViewController: UIViewController {
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
+        print(sender.tag)
+        sender.alpha = 0.5
+        sender.setTitleColor(sender.titleColor(for: .normal)?.withAlphaComponent(0.5), for: .normal)
+
+      
+        UIView.animate(withDuration: 0.6) {
+            sender.alpha = 1.0
+            sender.setTitleColor(sender.titleColor(for: .normal)?.withAlphaComponent(1.0), for: .normal)
+        }
+        
         if currentQuestion < questions.count - 1 {
             currentQuestion += 1
             updateUI()
             progressBar.progress = Float(currentQuestion) / Float(questions.count)
+        } else if currentQuestion == questions.count - 1{
+            progressBar.progress = 1
         }
     }
     
     func updateUI (){
-        questionLabel.text = questions[currentQuestion]
+        questionLabel.text = questions[currentQuestion].text
     }
     
 }
