74ef0314cd5555bd76f8f0803f0841bcfd4fe027
Jules <ju.pabst@gmail.com>
2023-12-31 13:38:25 +0100

    adapted user interface to be multiple choice

--- Quiz/Quiz/Model/QuizBrain-Logic.swift
+++ Quiz/Quiz/Model/QuizBrain-Logic.swift
@@ -8,18 +8,16 @@
 import Foundation
 
 struct QuizBrain {
-    let questions = [   Question(q: "A slug's blood is green.", a: 1),
-                        Question(q: "Approximately one quarter of human bones are in the feet.", a: 1),
-                        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: 1),
-                        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: 1),
-                        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: 0),
-                        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: 1),
-                        Question(q: "You can lead a cow down stairs but not up stairs.", a: 0),
-                        Question(q: "Google was originally called 'Backrub'.", a: 1),
-                        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: 1),
-                        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: 0),
-                        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: 0),
-                        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: 1)]
+    let questions = [    Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: 1),
+                         Question(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], correctAnswer: 2),
+                         Question(q: "What do the letters in the GMT time zone stand for?", a: ["Global Meridian Time", "Greenwich Mean Time", "General Median Time"], correctAnswer: 1),
+                         Question(q: "What is the French word for 'hat'?", a: ["Chapeau", "Écharpe", "Bonnet"], correctAnswer: 0),
+                         Question(q: "In past times, what would a gentleman keep in his fob pocket?", a: ["Notebook", "Handkerchief", "Watch"], correctAnswer: 2),
+                         Question(q: "How would one say goodbye in Spanish?", a: ["Au Revoir", "Adiós", "Salir"], correctAnswer: 1),
+                         Question(q: "Which of these colours is NOT featured in the logo for Google?", a: ["Green", "Orange", "Blue"], correctAnswer: 1),
+                         Question(q: "What alcoholic drink is made from molasses?", a: ["Rum", "Whisky", "Gin"], correctAnswer: 0),
+                         Question(q: "What type of animal was Harambe?", a: ["Panda", "Gorilla", "Crocodile"], correctAnswer: 1),
+                         Question(q: "Where is Tasmania located?", a: ["Indonesia", "Australia", "Scotland"], correctAnswer: 1)]
     
     var currentQuestion = 0
     
